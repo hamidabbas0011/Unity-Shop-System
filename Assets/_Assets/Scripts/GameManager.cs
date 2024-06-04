18927987324398bd732fbd554f06dcfe271940ff
Hamid Abbas <47805267+hamidabbas0011@users.noreply.github.com>
2024-06-04 18:25:54 +0530

    Second-Commit

--- Assets/_Assets/Scripts/GameManager.cs
+++ Assets/_Assets/Scripts/GameManager.cs
@@ -1,28 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 public class GameManager : MonoBehaviour
 {
     public GameObject coinPrefab;
     public TextMeshProUGUI coinsText;
+    public TextMeshProUGUI[] itemCountTexts;
+    public int[] itemCounts;
     public int coinCount = 0;
     public float spawnAreaMin = -44f;
     public float spawnAreaMax = 44f;
     public Transform _coinParent;
+    public Button[] itemButtons;
+    public int[] itemPrices;
+    
 
     
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnCoin());
+        coinCount = 10000;
+        for (int i = 0; i < itemButtons.Length; i++)
+        {
+            int index = i; // Prevent closure issue
+            itemButtons[i].onClick.AddListener(() => PurchaseItem(index));
+        }
+        UpdateItemCountsUI();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
     }
+    void UpdateItemCountsUI()
+    {
+        for (int i = 0; i < itemCountTexts.Length; i++)
+        {
+            itemCountTexts[i].text = $"{itemCounts[i]}";
+        }
+    }
+    
+    //--------------------Shop-System--------------------
+    void PurchaseItem(int itemIndex)
+    {
+        if (coinCount >= itemPrices[itemIndex])
+        {
+            coinCount -= itemPrices[itemIndex];
+            itemCounts[itemIndex]++;
+
+            coinsText.text = $"Coins: {coinCount}";
+            Debug.Log("Purchased Item " + (itemIndex + 1));
+            UpdateItemCountsUI();
+        }
+        else
+        {
+            Debug.Log("Not enough currency to purchase Item " + (itemIndex + 1));
+        }
+    }    
+    
+    //--------------------Shop-System-Ends--------------------
     private IEnumerator SpawnCoin()
     {
         int x = Random.Range(1, 6);
@@ -42,4 +83,6 @@ public class GameManager : MonoBehaviour
                 
         }
     }
+    
+    
 }
